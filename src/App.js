4e6e83189a4d14e3820d056a593520dce6987877
jpandiscia1709 <jpangithub1709@gmail.com>
2021-10-08 17:46:36 -0400

    attempting to establish connection with API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+  state = {
+    songs:[]
+  }
+
+  componentDidMount(){
+    this.getAllSongs();
+  }
+
+  async getAllSongs(){
+    let response = await axios.get('http://127.0.0.1:8000/music/');
+   console.log(response.data)
+   this.setState({
+      songs: response.data
+    });
+  }
+
+  
 }
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <h1>Music Library</h1>
+//         <p>
+//           <h2>Table of songs here</h2>
+//         </p>
+//         <h3>form to add songs</h3>
+//       </header>
+//     </div>
+//   );
+// }
+
 
 export default App;
