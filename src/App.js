3374f4722d04c9cc343d3f89f5089f0c73d1a9f7
jpandiscia1709 <jpangithub1709@gmail.com>
2021-10-11 12:22:57 -0400

    established API connection to database

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 import React, { Component } from 'react';
@@ -11,35 +11,39 @@ class App extends Component {
   }
 
   componentDidMount(){
+    // axios.get('http://127.0.0.1:8000/music/')
+    // .then(response => this.setState ({
+    //   songs: response.data
+    // }));
+
+
     this.getAllSongs();
   }
 
   async getAllSongs(){
-    let response = await axios.get('http://127.0.0.1:8000/music/');
+  let response = await axios.get('http://127.0.0.1:8000/music/');
    console.log(response.data)
    this.setState({
       songs: response.data
     });
   }
 
-  
+
+
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Music Library</h1>
+          <p>
+            Table of songs here
+          </p>
+          <h3>form to add songs</h3>
+        </header>
+      </div>
+    );  
+  }
 }
-
-
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <h1>Music Library</h1>
-//         <p>
-//           <h2>Table of songs here</h2>
-//         </p>
-//         <h3>form to add songs</h3>
-//       </header>
-//     </div>
-//   );
-// }
 
 
 export default App;
